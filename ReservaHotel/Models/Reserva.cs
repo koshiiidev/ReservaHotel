edeb09092b93cad114657af6ed1d27c1686d26e3
KoshiiiDev <saaclv27@gmail.com>
2024-12-03 14:55:03 -0600

    Creacion del Proyecto, base de datos y las vistas y controladores

---
+++ ReservaHotel/Models/Reserva.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ReservaHotel.Models;
+
+public partial class Reserva
+{
+    public int Id { get; set; }
+
+    public string NombreCliente { get; set; } = null!;
+
+    public DateOnly FechaInicio { get; set; }
+
+    public DateOnly FechaFin { get; set; }
+
+    public int NumeroHabitacion { get; set; }
+
+    public string Estado { get; set; } = null!;
+}
